7d49123a66a46a91eea17a21227de0b613be1891
KanakamSasikalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-05 21:57:28 +0530

    Update comments.sh

--- comments.sh
+++ comments.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-#This is single line comment
-#Single line comments are represnted by using Hash(#) symbol
-#These comments are ignored by interpreter or compiler
+#Comments in Bash is represented by the symbol Hash( # ) followed by statement
 
+#For Example
+#This is a comment in Bash
 
-<<COMMENT
-	  Multiline comments
- COMMENT
+#Display this is a comment on screen
+
+echo "The Comments are represented by using #(hash) symbol"
+echo "#This is a comment" 
+
+#The comments are generally ignored by the compiler or interpreter
+
+#These are not executed by the command line interpreter
+
+#The comments generally used for more readablility to the programmer and new user
 
