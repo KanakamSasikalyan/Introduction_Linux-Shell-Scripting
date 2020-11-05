d3011b9fc42e1b55ae1126a9b67d092e654d7b15
KanakamSasikalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-05 22:06:40 +0530

    Update Display.sh

--- Display.sh
+++ Display.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+#Script starts Here
+echo "Scripts Starts Here !"
+
+#Display a statement on the screen
 echo "Shell scipting is one of the most important techniqe used in Linux"
+
+#End of the script
+echo "Script Ended!"
 
