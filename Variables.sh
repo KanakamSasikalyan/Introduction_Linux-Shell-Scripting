0fa525511f7d3cdc849c9b97e2da637181e6d6d2
KanakamSasikalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-05 22:10:46 +0530

    Update Variables.sh

--- Variables.sh
+++ Variables.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+#Script starts Here
+echo "Script Starts Here!"
+
+#Variable creation
 SKILL="Shell Scripting"
+
+#Displaying the variable created in the statement
 echo "${SKILL} is one of the most important to develope Linx Kernal"
+
+#Here to access the value from variable you created use the $(dollar) symbol followed curly braces {__variable you created__}
+
+#Script ended
+echo "Script Ended"
