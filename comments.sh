b8dcea7ec22b0af71e82869eb251a49852ae8b4a
KanakamSasikalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-05 21:55:11 +0530

    Add files via upload

---
+++ comments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This is single line comment
+#Single line comments are represnted by using Hash(#) symbol
+#These comments are ignored by interpreter or compiler
+
+
+<<COMMENT
+	  Multiline comments
+ COMMENT
+
