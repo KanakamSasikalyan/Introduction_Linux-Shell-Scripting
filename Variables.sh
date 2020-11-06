b6419f1af7a58fba5d29b31f3f0108c74b275460
SasiKalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-06 11:16:00 +0530

    Update Variables.sh

--- Variables.sh
+++ Variables.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
+#---------------------------------------------------------------------------------------
+#Rules for naming the variables                                                        
+#1) Variable name must begin with Alphanumeric character or underscore character ( _ ) 
+#Accepted Variables :                                                                  
+                    #__ABC                                                             
+                    #Abc                                                               
+                    #A2B
+                    #ABC
+#Unacceptable Variables :
+                    #123
+                    #A-BC
+                    #1A2B
 
+#2) Don't put spaces on either side of the equal sign when assigning value to variable.
+#Example:
+        #Acceptable:
+                  #VAR=10
+                  #SYS="Linux"
+                  #_ABC=26
+                  
+        #Unacceptable:
+                  #VAR = 10
+                  #SYS = "Linux"
+                  #_ABC = 26
+                  
+#3) Variables are case-sensitive, just like filename in Linux. For e.g.
+# no=10
+# No=11
+# NO=20
+# nO=2 
+
+#4) Do not use ?,* etc, to name your variable names.
+ #---------------------------------------------------------------------------------------                  
 #Script starts Here
 echo "Script Starts Here!"
 
