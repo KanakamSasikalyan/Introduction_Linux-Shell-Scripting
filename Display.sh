90552e1fa524d04f869365c51b2a584cbb1c90b8
SasiKalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-06 11:53:04 +0530

    Add files via upload

---
+++ Display.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Script starts here
+echo "Script started!"
+
+#Usage of the simple echo
+
+echo "Hello World!"
+
+#Echo without a trailing a new line
+
+echo -n "Hello World!"
+
+#Echo with interpreting backslash escaped characters
+
+echo -e "Hello\nWorld!"
+
+#Echo without interpreting backslash escaped characters
+
+echo -E "Hello\nWorld!"
+
+#End of the script
+
+echo "Script Ended!"
