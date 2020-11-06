0faec36cbe5846d94169cf7e4f60b68ec27f7571
SasiKalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-06 12:06:42 +0530

    Add files via upload

---
+++ user_input.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This script demonstrates the reading the input from the user
+
+#Script starts
+echo "Script started"
+
+#Read the input from the user
+echo "Please Enter your name and age"
+read name age
+
+#Display the reading input on screen
+echo "You Sucessfully Entered the details"
+echo "Your are  $name and your age is $age"
+
+#Script ended
+echo "Script ended"
