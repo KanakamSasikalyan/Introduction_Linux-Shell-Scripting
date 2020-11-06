5ee86f688be2f26ef4ebbd1ad0613ed95814d8c3
SasiKalyan <67740644+KanakamSasikalyan@users.noreply.github.com>
2020-11-06 12:41:22 +0530

    Add files via upload

---
+++ Bash_Arithmetic_operators.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#This Script demonstrate the Bash Arithmetic Operators
+
+#Script Starts here
+echo -e  "Script Started\n"
+
+#Dispaly Arithmetic Operators
+echo -e "Arithmetic Operators in Bash\n"
+
+#Read two values from the user
+read -p "Enter the A Value" A
+read -p "Enter the B Value" B
+
+#Arithmetic operations goes here
+C=$((A+B))
+echo -e  "Addition : $C\n"
+
+C=$((A-B))
+echo -e "Subtractio : $C\n"
+
+C=$((A*B))
+echo -e "Multiplication : $C\n"
+
+C=$((A/B))
+echo -e "Division : $C\n"
+
+C=$((A%B))
+echo -e "ModuloDivision : $C\n"
+
+C=$((A**B))
+echo -e "Power or Exponentation : $C\n"
+
+#End of the script
+echo "End of script"
